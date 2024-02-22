8620884895c7c42bb9f90edaa9f00019b7c39c5b
Anbazhagan S <34671765+AnbazhaganSekar@users.noreply.github.com>
2024-02-22 10:47:47 +0530

    Create efs-provider-variables.tf

---
+++ efs-provider-variables.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  profile = "anbu"
+  region  = "ap-south-1"
+  version = ">= 4.45.0"
+}
+
+provider "kubernetes" {}
+
+data "aws_efs_file_system" "demo" {
+    creation_token = "efs-eks"
+}
+
+output "demo" {
+    value = data.aws_efs_file_system.demo
+}
+
+resource "kubernetes_namespace" "demo" {
+  metadata {
+    name = "terraform-prom-graf-namespace"
+  }
+}
